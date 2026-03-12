88bedbc336026edb9d2537a88c5fd4b72fd4dde0
KamiyeL <leila.kamiye@gmail.com>
2026-03-12 21:03:00 +0100

    task48: trmnl menue,device visible in ui

--- addons/trmnl/__manifest__.py
+++ addons/trmnl/__manifest__.py
@@ -3,6 +3,10 @@
     "version": "1.0",
     "summary": "Integration von TRMNL e-Ink Displays in Odoo",
     "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/trmnl_device_views.xml",
+    ],
     "installable": True,
     "application": True,
 }

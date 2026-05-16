1ebae06a1142a7a44cf5ac5f87390fe986007fc3
KamiyeL <leila.kamiye@gmail.com>
2026-05-16 16:35:15 +0200

    rebase

--- addons/trmnl/models/trmnl_device_display.py
+++ addons/trmnl/models/trmnl_device_display.py
@@ -62,15 +62,6 @@ class TrmnlDeviceDisplayMixin(models.Model):
             status = DEFAULT_DISPLAY_ERROR_STATUS
         return {"status": status}
 
-    def _consume_identify_flag(self):
-        """Consume the one-shot identify flag if set."""
-        self.ensure_one()
-
-        if self.identify_pending:
-            self.write({"identify_pending": False})
-            return True
-        return False
-
     def build_display_response(self):
         """Build the normal display payload for an accepted device."""
         self.ensure_one()
